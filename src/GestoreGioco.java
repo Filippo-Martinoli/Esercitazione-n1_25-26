332bfd2f3c43583588f3c79e9fd00a57abf24f03
Gaia Mincigrcci <gaia.mincigrucci@gmail.com>
2025-10-16 13:26:10 +0200

    Caricamento progetto

---
+++ src/GestoreGioco.java
@@ -0,0 +1,18 @@
+public class GestoreGioco {
+    private int numeroVincente;
+
+
+    public GestoreGioco() {
+        numeroVincente = 10;
+    }
+
+    public void verifica(int num) {
+        if (num == numeroVincente) {
+            //verifica num
+            System.out.println("Il numero è giusto, BRAVO!!!");
+
+        } else {
+            System.out.println("Il numero è sbagliato, riprova!!!");
+        }
+    }
+}
