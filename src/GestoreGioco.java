ad3cf744e604260ed6d768e341669473bdbc49c1
Filippo-Martinoli <filippomartinoli07@gmail.com>
2025-10-27 10:57:02 +0100

    Update GestoreGioco.java

--- src/GestoreGioco.java
+++ src/GestoreGioco.java
@@ -1,16 +1,33 @@
+/**
+ * La classe {@code GestoreGioco} gestisce un semplice gioco
+ * in cui l'utente deve indovinare un numero vincente.
+ */
 public class GestoreGioco {
+
+    /**
+     * Il numero che rappresenta la combinazione vincente del gioco.
+     */
     private int numeroVincente;
 
-
+    /**
+     * Costruttore della classe {@code GestoreGioco}.
+     * Inizializza il numero vincente a 10.
+     */
     public GestoreGioco() {
         numeroVincente = 10;
     }
 
+    /**
+     * Verifica se il numero inserito dall'utente corrisponde
+     * al numero vincente. Stampa un messaggio in console che
+     * indica se il numero è corretto o meno.
+     *
+     * @param num il numero inserito dall'utente da confrontare con quello vincente
+     */
     public void verifica(int num) {
         if (num == numeroVincente) {
-            //verifica num
+            // verifica num
             System.out.println("Il numero è giusto, BRAVO!!!");
-
         } else {
             System.out.println("Il numero è sbagliato, riprova!!!");
         }
